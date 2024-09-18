976998b0cb29c07eee200cf44f3450373d9f6e46
Lucas <luccasvnz@gmail.com>
2024-09-17 21:30:46 -0300

    some changes

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchIcon = document.getElementById('search-icon');
-    const searchContainer = document.querySelector('.search-container');
-    
-    searchIcon.addEventListener('click', function() {
-        searchContainer.classList.toggle('active');
-    });
-});
